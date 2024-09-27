368829d6238beffd37b52244ed6069d948f66836
Benjamin23115 <benjaminfarias313@gmail.com>
2024-09-26 21:21:30 -0700

    update problem 5 with algos

--- problem5.c
+++ problem5.c
@@ -1,70 +1,82 @@
-/* Cpt S 411, Introduction to Parallel Computing
-* School of Electrical Engineering and Computer Science
-*
-* Example code
-* Send receive test:
-* rank 1 sends to rank 0 (all other ranks sit idle)
-* For timing use of C gettimeofday() is recommended.
-this is a draft funcion for timing
-unsigned long utime(void)
-{
-struct timeval tv;
-unsigned long result = 0;
-gettimeofday(&tv, NULL);
-result += (tv.tv_sec * 1000000);
-result += tv.tv_usec;
-return result;
-}
-* */
 #include <stdio.h>
 #include <mpi.h>
 #include <assert.h>
 #include <sys/time.h>
-int main(int argc, char *argv[])
+#include <stdlib.h>
+
+void calculateTimeTaken(struct timeval start, struct timeval end, const char *messageType, int rank, int messageSize)
 {
-    int rank, p;
+    long tSend = (end.tv_sec - start.tv_sec) * 1000000000L + (end.tv_usec - start.tv_usec) * 1000;
+    printf("Rank =%d; Time taken %s message of size %d: %ld nanoseconds\n", rank, messageType, messageSize, tSend);
+}
+
+void calculateLatency(int rank, int dest)
+{
     struct timeval start, end;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
-    printf("my rank=%d\n", rank);
-    printf("Rank=%d: number of processes =%d\n", rank, p);
-    assert(p >= 2);
+
     if (rank == 1)
     {
-        int x = 10, y = 0;
-        int dest = 0;
-        // sending
+        int x = 1;
         gettimeofday(&start, NULL);
         MPI_Send(&x, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
         gettimeofday(&end, NULL);
-        double tSend = (end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) / 1000;
-        printf("Rank=%d: sent message %d to rank %d; Send time %.2f millisec\n", rank, x, dest, tSend);
-        // receiving
-        MPI_Status status;
-        gettimeofday(&start, NULL);
-        MPI_Recv(&y, 0, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-        gettimeofday(&end, NULL);
-        double tRecv = (end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) / 1000;
-        printf("Rank=%d: received message %d from rank %d; Recv time %.2f millisec\n", rank, y, status.MPI_SOURCE, tRecv);
+        calculateTimeTaken(start, end, "Latency sending", rank, sizeof(x));
     }
     else if (rank == 0)
     {
         int y = 0;
         MPI_Status status;
-        // receiving
         gettimeofday(&start, NULL);
         MPI_Recv(&y, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
         gettimeofday(&end, NULL);
-        double tRecv = (end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) / 1000;
-        printf("Rank=%d: received message %d from rank %d; Recv time %.2f millisec\n", rank, y, status.MPI_SOURCE, tRecv);
-        // sending
-        gettimeofday(&start, NULL);
-        MPI_Send(&y, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-        gettimeofday(&end, NULL);
-        double tSend = (end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) / 1000;
-        printf("Rank=%d: sent message %d to rank %d; Send time %.2f millisec\n", rank, y, 1, tSend);
+        calculateTimeTaken(start, end, "Latency receiving", rank, sizeof(y));
     }
+}
+
+void calculateBandwidth(int rank, int dest, int messageSize)
+{
+    struct timeval start, end;
+    char *message = malloc(messageSize * sizeof(char));
+    for (int i = 0; i < messageSize; i++)
+    {
+        message[i] = 'a';
+    }
+    if (rank == 1)
+    {
+        gettimeofday(&start, NULL);
+        MPI_Send(message, messageSize, MPI_CHAR, dest, 0, MPI_COMM_WORLD);
+        gettimeofday(&end, NULL);
+        calculateTimeTaken(start, end, "Bandwith sending", rank, messageSize);
+    }
+    else if (rank == 0)
+    {
+        MPI_Status status;
+        gettimeofday(&start, NULL);
+        MPI_Recv(message, messageSize, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        gettimeofday(&end, NULL);
+        calculateTimeTaken(start, end, "Bandwidth receiving", rank, messageSize);
+    }
+
+    free(message);
+}
+
+int main(int argc, char *argv[])
+{
+    int rank, p;
+    struct timeval start, end;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    assert(p >= 2);
+    int messageSizes[] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
+
+    calculateLatency(rank, 1 - rank);
+    for (int i = 0; i < 11; i++)
+    {
+        calculateBandwidth(rank, 1 - rank, messageSizes[i]);
+    }
+
     MPI_Finalize();
     return 0;
 }
